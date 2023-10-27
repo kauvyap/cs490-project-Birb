546bdcbd768c43ee5d2234562e71d0e9543a5851
Guilherme Vilatoro Taglianeti <gv8@njit.edu>
2023-10-27 16:37:49 -0400

    Added a homepage and a basic profile page

--- crush-it/src/App.js
+++ crush-it/src/App.js
@@ -6,6 +6,8 @@ import Navbar from "./components/navbar";
 import RecordList from "./components/recordList";
 import Edit from "./components/edit";
 import Create from "./components/create";
+import Homepage from "./Homepage/index";
+import Profile from "./Profile";
  const App = () => {
  return (
    <div>
@@ -14,6 +16,8 @@ import Create from "./components/create";
        <Route exact path="/" element={<RecordList />} />
        <Route path="/edit/:id" element={<Edit />} />
        <Route path="/create" element={<Create />} />
+       <Route path="/homepage" element={<Homepage/>} />
+       <Route path="/profile" element={<Profile/>} />
      </Routes>
    </div>
  );
