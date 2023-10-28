4b636b4702a5c82ccc1941ea1528f0467b53f925
Guilherme Vilatoro Taglianeti <gv8@njit.edu>
2023-10-28 15:26:31 -0400

    Finished the sidebar and navbar, also changed the app.js file to make the side bar and nav bar work

--- crush-it/src/components/sidebar.js
+++ crush-it/src/components/sidebar.js
@@ -4,18 +4,26 @@ import "bootstrap/dist/css/bootstrap.css";
  // We import NavLink to utilize the react router.
 import navbar from "./navbar2";
 import { NavLink } from "react-router-dom";
-import { Box, VStack, Text, Button,ChakraProvider } from "@chakra-ui/react";
+import { Box, VStack, Text, Button,ChakraProvider, Spacer, createIcon, Image} from "@chakra-ui/react";
 
-
+const Group_2 = createIcon({
+    displayName: "Group_2",
+    viewBox: "0 0 148 148",
+    d: "M148 113.19H0v.074h148v-.074z",
+    });
  // Here, we display our Navbar
 export default function Sidebar() {
       return (
         
-        <VStack align="start" height={"100vh"} width="55" spacing={4} p={4} bg="gray.200">
-          <Text fontSize="30xl">Crush It</Text>
-          <Button variant="outline">Item 1</Button>
-          <Button variant="outline">Item 2</Button>
-          <Button variant="outline">Item 3</Button>
+        <VStack align="start" height={"100vh"} width="200px" spacing={4} p={4} bg="black" alignItems={"center"}>
+          <Text fontSize="30px" fontWeight={"400"} textColor={"white"} align={"center"} fontFamily={"Fredoka One"}>Crush It</Text>
+          
+          <Box height={"1px"} width={"160px"} bg={"#3E3F42"}></Box>
+
+          <Image textColor="white" src="/smallLogo.svg" alt="SVG Image" />
+          
+          <Text  fontFamily={"DM Sans"} textAlign={"center"} fontSize={"20px"} textColor={"white"} fontWeight={"700"}>It's time to plan your day!</Text>
+          <Button height={"54px"} borderRadius={"14px"} variant="outline" color={"white"} fontSize={"18px"} fontWeight={"700"} width={"160px"}>Plan Day</Button>
         </VStack>
         
       );
