7bac73938f42587c222c4a14b14a2cdc9e6fe10b
shailis17 <shailissoni@gmail.com>
2023-11-04 18:33:51 -0400

    build temporary modal as placeholder for task form

--- crush-it/src/Homepage/addtask.js
+++ crush-it/src/Homepage/addtask.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import { IconButton } from '@chakra-ui/react';
+import { useDisclosure } from "@chakra-ui/react";
+import { IconButton, Button, Text, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton } from '@chakra-ui/react';
 import { AddIcon } from "@chakra-ui/icons";
 
 function AddTask() {
+
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
     return (
-        <IconButton isRound={true} variant='solid' aria-label='Done' fontSize='15px' fontWeight={"extrabold"} icon={<AddIcon />} ml={4} mb={1.5}
+        <>
+        <IconButton onClick={onOpen} isRound={true} variant='solid' aria-label='Done' fontSize='15px' fontWeight={"extrabold"} icon={<AddIcon />} ml={4} mb={1.5}
                         colorScheme="blue" style={{ background: 'linear-gradient(#5D8EFF 100%, #3E6FE1 100%)', color: 'white' }}/>
 
-    
+                        <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
+                        <ModalOverlay />
+                        <ModalContent>
+                          <ModalHeader>Modal Title</ModalHeader>
+                          <ModalCloseButton />
+                          <ModalBody>
+                            <Text fontWeight='bold' mb='1rem'>
+                              You can scroll the content behind the modal
+                            </Text>
+                          </ModalBody>
+                
+                          <ModalFooter>
+                            <Button colorScheme='blue' mr={3} >
+                              Save
+                            </Button>
+                            <Button variant='ghost'  onClick={onClose}>Cancel</Button>
+                          </ModalFooter>
+                        </ModalContent>
+                      </Modal>
+        </>  
   );
 };
 
