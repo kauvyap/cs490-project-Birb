bcf8b64c928653941a231ff21ab4b77042f41519
shailis17 <shailissoni@gmail.com>
2023-11-08 10:19:58 -0500

    jest still not working - DO NOT MERGE

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,29 @@
+//module.exports = {
+  //  preset: 'ts-jest',
+   // testEnvironment: 'jsdom',
+    //testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(test).ts?(x)"],
+//    transform: {
+  //      "^.+\\.(js|ts)$": "ts-jest",
+   // },
+ //   transformIgnorePatterns: [
+  //      "/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.js$",
+   //     "/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.ts$",
+     //   "/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.tsx$",
+    //],
+//}
+
 module.exports = {
     preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(test).ts?(x)"],
-    transform: {
-        "^.+\\.(js|ts)$": "ts-jest",
-    },
-    transformIgnorePatterns: [
-        "/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.js$",
-        "/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.ts$",
-        "/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.tsx$",
-    ],
-}
+    //testEnvironment: 'node', // or your preferred environment
+    // Other Jest configuration options...
+    //testEnvironment: 'jsdom',
+
+    //preset: 'ts-jest',
+    testEnvironment: 'jest-environment-jsdom',
+  
+    // An array of file paths or globs that specify which test files to run.
+    testMatch: ['**/__tests__/**/*.test.js'],
+  
+    // An array of file extensions that are considered test files.
+    moduleFileExtensions: ['js', 'mjs', 'jsx', 'json'],
+  };
