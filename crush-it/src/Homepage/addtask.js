7726f30011ef770da37b04b0f402d7c40b5b401a
Guilherme Vilatoro Taglianeti <gv8@njit.edu>
2023-11-10 01:08:07 -0500

    Fixed bug where user could add task with no title and priority

--- crush-it/src/Homepage/addtask.js
+++ crush-it/src/Homepage/addtask.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { useDisclosure } from "@chakra-ui/react";
+import { FormErrorMessage, useDisclosure } from "@chakra-ui/react";
 import { IconButton, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, Input, NumberInput, NumberInputField, NumberIncrementStepper, NumberDecrementStepper, NumberInputStepper, Textarea, Select, FormControl, FormLabel, VStack, HStack} from '@chakra-ui/react';
 import { AddIcon } from "@chakra-ui/icons";
 
@@ -11,6 +11,8 @@ function AddTask(props) {
     const [description, setDescription] = useState('');
     const [priority, setPriority] = useState('');
     const [timers, setTimers] = useState(1);
+    const [priorityError, setPriorityError] = useState(false);
+    const [titleError, setTitleError] = useState(false);
 
     // Delete unsaved task creation values
     useEffect(() => {
@@ -29,6 +31,8 @@ function AddTask(props) {
     }, )
     const onSubmit = async (e) => {
       e.preventDefault();
+      setPriorityError(false);
+      setTitleError(false);
       var topTasks = []
       var importantTasks = []
       var otherTasks = []
@@ -36,7 +40,12 @@ function AddTask(props) {
       .then(res => res.json())
       .then(data => {topTasks = data.topTasks; importantTasks = data.importantTasks; otherTasks = data.otherTasks})
       .catch((err) => console.log(err))
-      
+
+      if(!title.length > 0){
+        setTitleError(true);
+        return -1;
+      }
+
       if (priority === 'Top') {
         if (topTasks[0] === null) {
           topTasks = [{dateAssigned: props.dateSelected, title: title, description: description, priority: priority, pomodoroTimers: timers, status: 'NS'}]  
@@ -44,19 +53,23 @@ function AddTask(props) {
           topTasks.push({dateAssigned: props.dateSelected, title: title, description: description, priority: priority, pomodoroTimers: timers, status: 'NS'})
         }        
       }
-      if (priority === 'Important') {
+      else if (priority === 'Important') {
         if (importantTasks[0] === null) {
           importantTasks = [{dateAssigned: props.dateSelected, title: title, description: description, priority: priority, pomodoroTimers: timers, status: 'NS'}]  
         } else {
           importantTasks.push({dateAssigned: props.dateSelected, title: title, description: description, priority: priority, pomodoroTimers: timers, status: 'NS'})
         }
       }
-      if (priority === 'Other') {
+      else if (priority === 'Other') {
         if (otherTasks[0] === null) {
           otherTasks = [{dateAssigned: props.dateSelected, title: title, description: description, priority: priority, pomodoroTimers: timers, status: 'NS'}]  
         } else {
           otherTasks.push({dateAssigned: props.dateSelected, title: title, description: description, priority: priority, pomodoroTimers: timers, status: 'NS'})
         }
+      }
+      else{
+        setPriorityError(true);
+        return -1;
       }
 
       await fetch('http://localhost:5000/api/tasks/' + props.user, {
@@ -87,13 +100,15 @@ function AddTask(props) {
                         <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
                         <ModalOverlay />
                         <ModalContent>
-                          <ModalHeader> {props.dateSelected} - Add A New Task</ModalHeader>
+                          <ModalHeader> {props.dateSelected} {priority} - Add A New Task</ModalHeader>
                           <ModalCloseButton />
                           <ModalBody>
                           <VStack spacing={4}>
-                            <FormControl>
+                            <FormControl isInvalid={titleError} isRequired>
                             <FormLabel>Title</FormLabel>
-                            <Input type="text" placeholder="Enter task title" value={title} onChange={(e) => setTitle(e.target.value)}/>
+                            <Input type="text"  placeholder="Enter task title" value={title} onChange={(e) => setTitle(e.target.value)}/>
+                            {titleError && (<FormErrorMessage>A title is required</FormErrorMessage>)}
+
                             </FormControl>
 
                             <FormControl>
@@ -106,16 +121,17 @@ function AddTask(props) {
                             />
                             </FormControl>
 
-                            <HStack w={'100%'}>
-                            <FormControl>
+                            <HStack w={'100%'} align={"flex-start"} >
+                            <FormControl isInvalid={priorityError} isRequired>
                             <FormLabel>Priority</FormLabel>
-                            <Select placeholder='Select priority' value={priority} onChange={(e) => setPriority(e.target.value)}>
+                            <Select placeholder='Select priority'  value={priority} onChange={(e) => setPriority(e.target.value)}>
                                 <option value="Top">Top</option>
                                 <option value="Important" selected>
                                 Important
                                 </option>
                                 <option value="Other">Other</option>
                             </Select>
+                            {priorityError && (<FormErrorMessage>Select a valid option.</FormErrorMessage>)}
                             </FormControl>
   
                             <FormControl>
