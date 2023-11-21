49edf60fd39faa6db262a892f9a9b60b3a327220
shailis17 <shailissoni@gmail.com>
2023-11-21 18:14:21 -0500

    dark mode

--- crush-it/src/Homepage/focustime.js
+++ crush-it/src/Homepage/focustime.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { HStack, Spacer, useDisclosure } from "@chakra-ui/react";
 import { IconButton, Button, Text, Box, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton,
-         Tab, TabList, TabPanel, TabPanels, Tabs, Flex} from '@chakra-ui/react';
+         Tab, TabList, TabPanel, TabPanels, Tabs, Flex, useColorModeValue} from '@chakra-ui/react';
 import { AddIcon } from "@chakra-ui/icons";
 
 function FocusTime() {
 
     const { isOpen, onOpen, onClose } = useDisclosure()
+    const bg = useColorModeValue("#F3F3F3", "#1a202c");
+    const blueTxt = useColorModeValue('#6284FF', '#90cdf4');
 
     return (
         <>
@@ -27,15 +29,15 @@ function FocusTime() {
                             </TabList>
                             <TabPanels>
                               <TabPanel>
-                                <Box bg = "#F5F7F9" alignContent={"center"} p={10} textAlign={"center"}>
+                                <Box bg = {bg} alignContent={"center"} p={10} textAlign={"center"}>
                                   <Text fontFamily={"DM Sans"} fontWeight={"bold"} fontSize={"100px"}>25:00</Text>
                                   <Button colorScheme="blue" size={"lg"}>
                                     Start
                                   </Button>
                                 </Box>
                                 <Text mt={5} mb={5} fontFamily={"DM Sans"} fontWeight={"bold"} fontSize={"20px"}>Task Title</Text>
-                                <Box bg = "#F5F7F9" p={5}>
-                                  <Text fontFamily={"DM Sans"} textColor={"#6284FF"} fontWeight={"bold"} fontSize={"16px"} mb={3}>Notes</Text>
+                                <Box bg = {bg} p={5}>
+                                  <Text fontFamily={"DM Sans"} textColor={blueTxt} fontWeight={"bold"} fontSize={"16px"} mb={3}>Notes</Text>
                                   <Text fontFamily={"DM Sans"} fontWeight={"bold"} fontSize={"12px"} mb={3}>
                                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                                   </Text>
@@ -46,13 +48,13 @@ function FocusTime() {
                                       <Text fontFamily="DM Sans" textColor="white" fontWeight="bold" fontSize="20px">
                                         Pomos:
                                       </Text>
-                                      <Text fontFamily="DM Sans" textColor="#6284FF" fontWeight="bold" fontSize="20px">
+                                      <Text fontFamily="DM Sans" textColor={blueTxt} fontWeight="bold" fontSize="20px">
                                         0/3
                                       </Text>
                                       <Text fontFamily="DM Sans" textColor="white" fontWeight="bold" fontSize="20px" ml={8}>
                                         Finish At:
                                       </Text>
-                                      <Text fontFamily="DM Sans" textColor="#6284FF" fontWeight="bold" fontSize="20px">
+                                      <Text fontFamily="DM Sans" textColor={blueTxt} fontWeight="bold" fontSize="20px">
                                         19:53 (1.4h)
                                       </Text>
                                     </HStack>
@@ -60,7 +62,7 @@ function FocusTime() {
                                 </Box>
                               </TabPanel>
                               <TabPanel>
-                                  <Box bg = "#F5F7F9" alignContent={"center"} p={10} textAlign={"center"}>
+                                  <Box bg = {bg} alignContent={"center"} p={10} textAlign={"center"}>
                                     <Text fontFamily={"DM Sans"} fontWeight={"bold"} fontSize={"100px"}>5:00</Text>
                                     <Button colorScheme="blue" size={"lg"}>
                                       Start
@@ -68,7 +70,7 @@ function FocusTime() {
                                   </Box>
                               </TabPanel>
                               <TabPanel>
-                                <Box bg = "#F5F7F9" alignContent={"center"} p={10} textAlign={"center"}>
+                                <Box bg = {bg} alignContent={"center"} p={10} textAlign={"center"}>
                                   <Text fontFamily={"DM Sans"} fontWeight={"bold"} fontSize={"100px"}>15:00</Text>
                                   <Button colorScheme="blue" size={"lg"}>
                                     Start
